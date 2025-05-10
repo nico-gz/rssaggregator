c053fc2a7e084809e72c3cf12c2ca87aeb971a0d
nico-gz <facundo98n@gmail.com>
2025-05-10 18:16:12 -0300

    added posts table

---
+++ sql/queries/posts.sql
@@ -0,0 +1,21 @@
+-- name: createPost :one
+INSERT INTO posts (id, created_at, updated_at, title, url, description, published_at, feed_id)
+VALUES (
+    $1,
+    $2,
+    $3,
+    $4,
+    $5,
+    $6,
+    $7,
+    $8
+)
+RETURNING *;
+
+-- name: getPostsForuser :many
+SELECT title, posts.url, description, posts.created_at, posts.updated_at
+FROM posts
+INNER JOIN feeds ON feeds.id = posts.feed_id
+INNER JOIN users ON users.id = feeds.user_id
+WHERE users.id = $1
+ORDER BY posts.created_at DESC;
