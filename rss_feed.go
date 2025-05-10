fe61ad9e04fc6066c9861fa2a1f0793b4334a7f0
nico-gz <facundo98n@gmail.com>
2025-05-10 11:57:27 -0300

    move fetchFeed definition to rss_feed file

--- rss_feed.go
+++ rss_feed.go
@@ -9,6 +9,22 @@ import (
 	"time"
 )
 
+type RSSFeed struct {
+	Channel struct {
+		Title       string    `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Item        []RSSItem `xml:"item"`
+	} `xml:"channel"`
+}
+
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
+}
+
 func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
 	httpClient := http.Client{
 		Timeout: 10 * time.Second,
